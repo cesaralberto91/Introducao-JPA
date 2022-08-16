6c51c066387c0bbaa590846e4889e4078ea7287a
César Alberto da Silva <cesar@ifsp.edu.br>
2022-08-16 12:40:21 -0300

    Relacionamento bidirecional produto e categoria

---
+++ src/TesteProdutoCategoria.java
@@ -0,0 +1,62 @@
+
+import br.edu.ifsp.pep.modelo.Categoria;
+import br.edu.ifsp.pep.modelo.Produto;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteProdutoCategoria {
+
+    public static void main(String[] args) {
+        
+        EntityManager em = Persistence
+                .createEntityManagerFactory("aula1PU")
+                .createEntityManager();
+        
+
+        Categoria categoria = new Categoria();
+        categoria.setDescricao("Informática");
+        
+        
+        Produto produto = new Produto();
+        produto.setDescricao("Teclado");
+        produto.setPreco(new BigDecimal(60.0));
+        produto.setQuantidade(100);
+        
+        produto.setCategoria(categoria);
+        
+        
+        em.getTransaction().begin();
+        
+        em.persist(categoria);
+        em.persist(produto);
+        
+        em.getTransaction().commit();
+        
+        
+        //JPQL
+        //Sempre pensar em classe
+        
+        //SELECT p FROM Produto p  
+        TypedQuery<Produto> query = em
+                .createQuery("SELECT p FROM Produto p", 
+                        Produto.class);
+        
+        List<Produto> produtos = query.getResultList();
+        for (Produto p : produtos) {
+            System.out.println("Descrição: " + p.getDescricao());
+            System.out.println("Categoria: " + p.getCategoria().getDescricao());
+        }
+        
+        
+        em.close();
+        
+    }
+
+}
