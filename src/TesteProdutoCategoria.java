59fe8bfec0d6bab45b77d28fa47cc0643ca3b40c
César Alberto da Silva <cesar@ifsp.edu.br>
2022-08-19 15:20:32 -0300

    teste

--- src/TesteProdutoCategoria.java
+++ src/TesteProdutoCategoria.java
@@ -5,9 +5,6 @@ import br.edu.ifsp.pep.modelo.Categoria;
 import br.edu.ifsp.pep.modelo.Produto;
 import java.math.BigDecimal;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 
 /**
  *
@@ -15,30 +12,66 @@ import javax.persistence.TypedQuery;
  */
 public class TesteProdutoCategoria {
 
+    private static ProdutoDAO produtoDAO = new ProdutoDAO();
+
     public static void main(String[] args) {
-        
+
+        adicionarProdutos();
+
+        reajustarPrecoProdutos(5);
+        exibirProdutos();
+
+    }
+
+    private static void teste() {
         Categoria categoria = new Categoria();
         categoria.setDescricao("Informática");
-        
+
         CategoriaDAO categoriaDAO = new CategoriaDAO();
         categoriaDAO.inserir(categoria);
-               
+
         Produto produto = new Produto();
         produto.setDescricao("Teclado");
         produto.setPreco(new BigDecimal(60.0));
         produto.setQuantidade(100);
-        
+
         produto.setCategoria(categoria);
-        
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+
         produtoDAO.inserir(produto);
-       
+
         List<Produto> produtos = produtoDAO.buscar();
         for (Produto p : produtos) {
             System.out.println("Descrição: " + p.getDescricao());
             System.out.println("Categoria: " + p.getCategoria().getDescricao());
         }
-               
+    }
+
+    private static void adicionarProdutos() {
+
+        CategoriaDAO categoriaDAO = new CategoriaDAO();
+        for (int i = 0; i < 10; i++) {
+            Categoria categoria = new Categoria();
+            categoria.setDescricao("Categoria " + i);
+            categoriaDAO.inserir(categoria);
+
+            Produto produto = new Produto();
+            produto.setDescricao("Produto " + i);
+            produto.setPreco(new BigDecimal(60.0 * (i + 1)));
+            produto.setQuantidade(100);
+
+            produto.setCategoria(categoria);
+
+            produtoDAO.inserir(produto);
+
+        }
+
+    }
+
+    private static void reajustarPrecoProdutos(double porcentagem) {
+
+    }
+
+    private static void exibirProdutos() {
     }
 
 }
